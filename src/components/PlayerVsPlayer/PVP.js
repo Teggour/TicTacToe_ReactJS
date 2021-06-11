a0e4b1a407eb5cf3b90fa718e85cdb9c7d40cfa3
Teggour <TeggourMalik@gmail.com>
2021-06-11 12:34:59 +0300

    add PVP mode

--- src/components/PlayerVsPlayer/PVP.js
+++ src/components/PlayerVsPlayer/PVP.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import style from './style.module.css'
+
 import clickURL from '../../sounds/click.mp3'
 import errorURL from '../../sounds/error.mp3'
+import restartURL from '../../sounds/restart.mp3'
+import homeURL from '../../sounds/home.mp3'
+
 
 function Game() {
     let [squares, setSquares] = useState(Array(9).fill(null))
     let [count, setCount] = useState(0)
+    let [win, setWin] = useState(false)
     let [messageTxt, setMessageTxt] = useState("Player X's turn:")
+
+    const winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
     function cellClick(event) {
         let cellNum = event.target.getAttribute('data')
         let allCells = squares
 
-        if (allCells[cellNum] === null) {
-            var clickSound = new Audio(clickURL)
+        if (win === false && allCells[cellNum] === null) {
+            let clickSound = new Audio(clickURL)
             clickSound.play()
 
             if (count % 2 === 0) {
@@ -29,9 +45,45 @@ function Game() {
             setSquares(squares = [...allCells])
         }
         else {
-            var errorSound = new Audio(errorURL)
+            let errorSound = new Audio(errorURL)
             errorSound.play()
         }
+        searchWinner()
+    }
+
+    function searchWinner() {
+        let currentPlayer =
+            (count % 2 === 0)
+                ? "X"
+                : "O"
+
+        for (let i = 0; i < winCombinations.length; i++) {
+            let winCombo = winCombinations[i]
+            let checker = 0
+            for (let j = 0; j < winCombo.length; j++) {
+                if (squares[winCombo[j]] === currentPlayer) {
+                    checker++
+                    if (checker === winCombo.length) {
+                        setMessageTxt(`Player ${currentPlayer}'s WIN!`)
+                        setWin(true)
+                    }
+                }
+            }
+        }
+    }
+
+    function restartClick() {
+        var restartSound = new Audio(restartURL)
+        restartSound.play()
+        setSquares(Array(9).fill(null))
+        setCount(0)
+        setWin(false)
+        setMessageTxt("Player X's turn:")
+    }
+
+    function menuClick() {
+        var homeSound = new Audio(homeURL)
+        homeSound.play()
     }
 
     return (
@@ -49,8 +101,8 @@ function Game() {
                 <div className={style.cell} onClick={cellClick} data="8">{squares[8]}</div>
             </div>
             <div className={style.buttons}>
-                <button className={style.restart}>Restart</button>
-                <button className={style.menu}>Main menu</button>
+                <button className={style.restart} onClick={restartClick}>Restart</button>
+                <button className={style.menu} onClick={menuClick}>Menu</button>
             </div>
         </React.Fragment>
     )
